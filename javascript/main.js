6300ae2b8eeb69ac523410e8739cb43b01e2d54a
Daniel-Amas <amasdaniel3@gmail.com>
2023-04-29 01:14:21 -0400

    working contact  form

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,55 @@
-// import {fetchImages} from "./fetch.js";
+// ACTIVE LINKS FOR SECTIONS
+let sections = document.querySelectorAll("section")
+let navLinks = document.querySelectorAll("header nav a")
 
-// const projectsImgs = fetchImages(); 
+window.onscroll = () => {
+    sections.forEach(section => {
+        let top = window.scrollY
+        let offset = section.offsetTop - 150
+        let height = section.offsetHeight
+        let id = section.getAttribute("id")
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(link =>{
+                link.classList.remove("active")
+                document.querySelector("header nav a[href*= "+ id +"]").classList.add("active")
+            })
+        }
+    })
+}
 
 
-
-// projectsImgs.array.forEach(img => {
-//     const projGalleryItem = document.createElement("div")
-//     projGalleryItem.classList.add("project-gallery-item")
+// SENDING EMAIL
+var form = document.getElementById("contact-form");
     
-//     document.getElementsByClassName("project-gallery").appendChild(projGalleryItem)
-
-//     const projImg = document.createElement("img")
-//     projImg.src = 
-// });
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.classList.add('success');
+      status.innerHTML = "Successfully Sent";
+      form.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+            status.classList.add('error');
+            status.innerHTML = "Oops! There was a problem submitting your form"
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+form.addEventListener("submit", handleSubmit)
 
